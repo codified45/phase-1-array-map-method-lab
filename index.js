8e8a8e45f3488484dd5d73f7baf13ad8caaa56d2
Jack Lach <jack.lach4@gmail.com>
2023-01-31 21:58:01 -0500

    test passes

--- index.js
+++ index.js
@@ -11,6 +11,62 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
+// titleCased(): returns an array with title case tutorial names. Note that this function takes no arguments and should use the global tutorials variable as data.
+
+// This version works by destructively modifying the tutorials array.  
+
 const titleCased = () => {
-  return tutorials
+
+  const titleCasingFunc = (tutElement) => {
+    const strArray = tutElement.split("");
+    let previousCharIsSpace = false;
+    let firstChar = strArray[0].toUpperCase();
+    strArray.splice(0, 1, firstChar);
+
+    for (let i = 1; i < strArray.length; i++) {
+      if (strArray[i] === " ") {
+        previousCharIsSpace = true;
+      } else if (previousCharIsSpace) {
+        strArray.splice(i, 1, strArray[i].toUpperCase());
+        previousCharIsSpace = false;
+      }
+    }
+
+    let newTutElement = strArray.join(""); // need the quotes in there or else you get commas in between every letter
+    tutorials.splice(tutorials.indexOf(tutElement), 1, newTutElement)
+  }
+
+  tutorials.map(titleCasingFunc);
+  return tutorials;
+}
+
+
+// This version works by creating a newTutorialsArray (non-destructive)
+
+const newTutorialsArray = [];
+
+const titleCasedFoo = () => {
+
+  const titleCasingFunc = (tutElement) => {
+    const strArray = tutElement.split("");
+    let previousCharIsSpace = false;
+    let firstChar = strArray[0].toUpperCase();
+    strArray.splice(0, 1, firstChar);
+
+    for (let i = 1; i < strArray.length; i++) {
+      if (strArray[i] === " ") {
+        previousCharIsSpace = true;
+      } else if (previousCharIsSpace) {
+        strArray.splice(i, 1, strArray[i].toUpperCase());
+        previousCharIsSpace = false;
+      }
+    }
+
+    let newTutElement = strArray.join(""); // need the quotes in there or else you get commas in between every letter
+    newTutorialsArray.push(newTutElement);
+  }
+
+  tutorials.map(titleCasingFunc);
+  return newTutorialsArray;
 }
